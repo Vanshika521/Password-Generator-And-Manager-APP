014a82b2b76094a6897f91c476dc024230987861
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-09-13 22:41:07 +0530

    Initial commit

---
+++ app/src/main/java/com/android_development/passwordgeneratorandmanagerapp/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.android_development.passwordgeneratorandmanagerapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
