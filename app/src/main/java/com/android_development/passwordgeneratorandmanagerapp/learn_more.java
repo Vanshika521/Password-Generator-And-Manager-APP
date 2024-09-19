4f7d6b3958246a6f28c3f8a24a920fbab6903c0c
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-09-19 22:22:16 +0530

    learn_more

--- app/src/main/java/com/android_development/passwordgeneratorandmanagerapp/learn_more.java
+++ app/src/main/java/com/android_development/passwordgeneratorandmanagerapp/learn_more.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,42 +12,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class learn_more extends AppCompatActivity {
 
+    TextView textView1,textView2;
     Button btn1,btn2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_learn_more);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,register.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        btn1.findViewById(R.id.btn1);
+        btn2.findViewById(R.id.btn2);
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,learn_more.class);
+                Intent intent = new Intent(learn_more.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
-
 
     }
 }
