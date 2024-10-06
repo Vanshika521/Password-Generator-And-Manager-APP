8af3a3a844b1c71d3ad3136f9cc5c5643a8d6668
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-10-06 21:53:26 +0530

    Password Strength,App Icon Updated

--- app/src/main/java/com/android_development/passwordgeneratorandmanagerapp/pwd_strength.java
+++ app/src/main/java/com/android_development/passwordgeneratorandmanagerapp/pwd_strength.java
@@ -2,6 +2,8 @@ package com.android_development.passwordgeneratorandmanagerapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,9 +17,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class pwd_strength extends AppCompatActivity {
 
-    TextView textView1,textView2;
+    TextView textView1, textView2;
     EditText pwd;
     Button btn;
+    Animation top;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +37,15 @@ public class pwd_strength extends AppCompatActivity {
         pwd = findViewById(R.id.pwd);
         btn = findViewById(R.id.btn);
         textView2 = findViewById(R.id.textView2);
+        top = AnimationUtils.loadAnimation(this,R.anim.top);
+
+        textView2.setAnimation(top);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String password= pwd.getText().toString().trim();
+                String password = pwd.getText().toString().trim();
 
                 if (password.isEmpty()) {
                     Toast.makeText(pwd_strength.this, "Enter Your Password", Toast.LENGTH_SHORT).show();
@@ -47,28 +53,23 @@ public class pwd_strength extends AppCompatActivity {
                         password.matches(".*[a-z].*") && // At least one lowercase letter
                         password.matches(".*[0-9].*") &&  // At least one digit
                         password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*")) {
-                    textView2.setText("Master-Level Security!");
+                    textView2.setText("🔐 Master-Level Security! 💪");
                 } else if (password.matches(".*[A-Z].*") && password.matches(".*[a-z].*") && password.matches(".*[0-9].*") ||
-                            password.matches(".*[A-Z].*") && password.matches(".*[a-z].*")  &&  password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*") ||
-                            password.matches(".*[A-Z].*") && password.matches(".*[0-9].*") &&  password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*")  ||
-                            password.matches(".*[a-z].*") && password.matches(".*[0-9].*")  &&  password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*")){
-                    textView2.setText("You're well-protected!");
+                        password.matches(".*[A-Z].*") && password.matches(".*[a-z].*") && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*") ||
+                        password.matches(".*[A-Z].*") && password.matches(".*[0-9].*") && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*") ||
+                        password.matches(".*[a-z].*") && password.matches(".*[0-9].*") && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*")) {
+                    textView2.setText("🔒 You're well-protected! 👍");
                 } else if (password.matches(".*[A-Z].*") && password.matches(".*[a-z].*") ||
-                            password.matches(".*[a-z].*") && password.matches(".*[0-9].*") ||
-                            password.matches(".*[0-9].*") &&  password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*")  ||
-                            password.matches(".*[A-Z].*")  &&  password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*")  ||
-                            password.matches(".*[a-z].*")  &&  password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*")  ||
-                            password.matches(".*[a-z].*")  &&  password.matches(".*[0-9].*")){
-                    textView2.setText("Decent, but could be stronger!");
+                        password.matches(".*[a-z].*") && password.matches(".*[0-9].*") ||
+                        password.matches(".*[0-9].*") && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*") ||
+                        password.matches(".*[A-Z].*") && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*") ||
+                        password.matches(".*[a-z].*") && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?.].*") ||
+                        password.matches(".*[a-z].*") && password.matches(".*[0-9].*")) {
+                    textView2.setText("⚠️ Decent, but could be stronger! 🛠️");
                 } else {
-                    textView2.setText("Exposed to Danger!");
+                    textView2.setText("❗ Exposed to Danger! 🛑");
                 }
             }
         });
-
-
     }
-
-
-
 }
