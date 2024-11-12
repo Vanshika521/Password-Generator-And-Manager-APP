5a41f74fd52c28aad82f15d465e5f6668d3c43c0
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-11-12 21:32:47 +0530

    Password Strength,App Icon Updated

--- app/src/main/java/com/android_development/passwordgeneratorandmanagerapp/MainActivity.java
+++ app/src/main/java/com/android_development/passwordgeneratorandmanagerapp/MainActivity.java
@@ -2,8 +2,6 @@ package com.android_development.passwordgeneratorandmanagerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
